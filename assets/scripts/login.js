4722a6d82d62057df4ef725a00f3c1da70813189
Andy <handy_4ndy@outlook.com>
2024-03-25 12:51:33 +0000

    Update login.js
    process.env removed from script file

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,4 +1,4 @@
-var xumm = new Xumm(process.env.XAMAN_APIKEY);
+var xumm = new Xumm(XAMAN_APIKEY);
 
 const burnRequestSigned = sessionStorage.getItem('burnRequestSigned');
 const isLoggedIn = sessionStorage.getItem('isLoggedIn');
